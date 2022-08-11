c7d18ddc9b420b0bba21ec62f61125b60524c62d
austin <villasanamallariaustin@gmail.com>
2022-08-12 00:38:25 +0800

    create viewsets view for 'main' serializers

--- main/views.py
+++ main/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import (
+    Tboperatorlist,
+    OptItemmastermain,
+    OptDbseikeimdrcrossout,
+)
+from .serializers import (
+    TboperatorlistSerializer,
+    OptItemmastermainSerializer,
+    OptDbseikeimdrcrossoutSerializer,
+)
 
-# Create your views here.
+class TboperatorlistViewsets (viewsets.ModelViewSet):
+    serializer_class = TboperatorlistSerializer
+    queryset = Tboperatorlist.objects.all()
+
+class OptItemmastermainViewsets (viewsets.ModelViewSet):
+    serializer_class = OptItemmastermainSerializer
+    queryset = OptItemmastermain.objects.all()
+
+class OptDbseikeimdrcrossoutViewsets (viewsets.ModelViewSet):
+    serializer_class = OptDbseikeimdrcrossoutSerializer
+    queryset = OptDbseikeimdrcrossout.objects.all()
