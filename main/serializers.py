e75267ac6583b1e12121caa20934a9fdcc0b154a
austin <villasanamallariaustin@gmail.com>
2022-08-12 00:36:19 +0800

    create serializers for 'main' models

---
+++ main/serializers.py
@@ -0,0 +1,24 @@
+from rest_framework import serializers
+from .models import (
+    Tboperatorlist,
+    OptItemmastermain,
+    OptDbseikeimdrcrossout,
+)
+
+class TboperatorlistSerializer (serializers.ModelSerializer):
+    
+    class Meta:
+        model = Tboperatorlist
+        fields = '__all__'
+
+class OptItemmastermainSerializer (serializers.ModelSerializer):
+    
+    class Meta:
+        model = OptItemmastermain
+        fields = '__all__'
+
+class OptDbseikeimdrcrossoutSerializer (serializers.ModelSerializer):
+    
+    class Meta:
+        model = OptDbseikeimdrcrossout
+        fields = '__all__'
